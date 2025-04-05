ab742236d0f1a738b5aad5ab1e90affa78568a6c
dev gajra <devgajra556@gmail.com>
2025-04-05 23:31:43 +0530

    payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ const name ="dev";
 
 const login="login page added";
 console.log(login);
+<<<<<<< HEAD
 const footer ="footer added in our website";
 console.log(footer);
+=======
+const payment= "integrated the payment gateway";
+console.log(payment);
+>>>>>>> f87baec (payment added)
