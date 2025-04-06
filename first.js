4079d0c5ecd5b47672f4857b3f674779c88ae7a1
dev gajra <devgajra556@gmail.com>
2025-04-06 13:06:40 +0530

    latest change

--- first.js
+++ first.js
@@ -4,10 +4,13 @@ const name ="dev";
 
 const login="login page added";
 console.log(login);
+const footer ="footer added in our website";
+console.log(footer);
 const payment= "integrated the payment gateway";
 console.log(payment);
->>>>>>> f87baec (payment added)
 
 const upi ="integrated the upi";
 console.log(upi);
+
+console.log("git hub par kaam start");
 
